5e3831ce09e961be26f138057880fb8d7da7e688
SindhuChaithra <sindhu.yepuri@vamstar.io>
2022-11-14 11:09:39 +0530

    read-JSON.py

---
+++ database.sql
@@ -0,0 +1,4 @@
+create table desk(id int, name varchar(20));
+insert into desk(id, name) values (1,'vamstar');
+insert into desk(id, name) values (2,'sindhu');
+select * from desk where name = 'vamstar'
