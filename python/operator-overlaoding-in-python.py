d6d7dd1a8a4d76d13d070e786e1acea8bef3a7ae
SindhuChaithra <sindhu.yepuri@vamstar.io>
2022-11-15 11:27:31 +0530

    operator-overloading-in-python.py

---
+++ python/operator-overlaoding-in-python.py
@@ -0,0 +1,22 @@
+# Python Program illustrate how
+# to overload an binary + operator
+# And how it actually works
+
+class A:
+	def __init__(vamstar, a):
+		vamstar.a = a
+
+	# adding two objects
+	def __add__(vamstar, o):
+		return vamstar.a + o.a
+ob1 = A(4)
+ob2 = A(8)
+ob3 = A("welcome")
+ob4 = A("company")
+
+print(ob1 + ob2)
+print(ob3 + ob4)
+
+#And can also be Understand as :
+print(ob1.__add__(ob2))
+print(ob3.__add__(ob4))
