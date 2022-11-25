1bd3523fb1226c3f0dd49bf7baf9d1d50499d5a6
SindhuChaithra <sindhu.yepuri@vamstar.io>
2022-11-25 17:06:57 +0530

    dictionary.py

--- practise/dictionary.py
+++ practise/dictionary.py
@@ -1,6 +1,11 @@
+# Dictionaries
 phonebook = {
     "abc" : 940123890,
     "Jack" : 9848757868,
     "any" : 947662781
 }
 print(phonebook)
+# Iterating over dictionaries
+phonebook = {"John" : 938477566,"Jack" : 938377264,"Jill" : 947662781}
+for name, number in phonebook.items():
+    print("Phone number of %s is %d" % (name, number))
