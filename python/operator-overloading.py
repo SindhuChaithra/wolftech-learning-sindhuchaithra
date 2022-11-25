c6108a4af6a1ecb27fc4e099e6a25ec8d08c754d
SindhuChaithra <sindhu.yepuri@vamstar.io>
2022-11-25 17:03:03 +0530

    operator-overloading.py

--- python/operator-overloading.py
+++ python/operator-overloading.py
@@ -1,10 +1,10 @@
-print(1 + 2)
-
 # concatenate two strings
+name = 'sindhu'
+# second_name = 'vamstar'
 print("sindhu" + ' ' "vamstar")
 
 # Product two numbers
 print(3 * 4)
 
 # Repeat the String
-print("vamstar" ''   '' '' * 4) # empty string with spaces gives space in the output sentences
+print("vamstar" ' '  ' ' ' ' * 4) # empty string with spaces gives space in the output sentences
