a67f9795e4b42aa5ec2dd2a0e3cfb4542b59042d
SindhuChaithra <sindhu.yepuri@vamstar.io>
2022-11-25 16:51:56 +0530

    python-basics.py

--- practise/python-basics.py
+++ practise/python-basics.py
@@ -1,2 +1,12 @@
-print(1)
-print('welcome to vamstar')
+print('hello world')
+# To define an integer,
+myint = 5
+print(myint)
+# To define an float value
+myfloat = 9.0
+print(myfloat)
+# Strings are defined either with a single quote or a double quotes.
+mystring = 'welcome'
+print(mystring)
+mystring = "hello"
+print(mystring)
